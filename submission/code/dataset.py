c610f2c570a79dc16a3c0477faa3aa22d87bbf53
Janez Justin <janez.justin0@gmail.com>
2022-05-26 17:54:05 +0200

    final dump

--- submission/code/dataset.py
+++ submission/code/dataset.py
@@ -1,3 +1,6 @@
+import cv2
+import json
+from matplotlib.pyplot import hist
 import torch
 from scipy.ndimage import gaussian_filter
 from torch.utils.data import Dataset
@@ -7,28 +10,55 @@ from torch.utils.data import random_split
 
 bolognese1 = [
     {'title': 'recording_2022_05_25_11_55_19',
-     'start': 200, 'end': 500, 'label': 0},  # 0 people
+     'start': 7, 'end': 18, 'label': 0},  # 0 people
     {'title': 'recording_2022_05_25_11_46_26',
-     'start': 200, 'end': 500, 'label': 1},  # 1 person
+     'start': 7, 'end': 18, 'label': 1},  # 1 person
     {'title': 'recording_2022_05_25_11_47_20',
-     'start': 200, 'end': 500, 'label': 2},  # 2 people
+     'start': 7, 'end': 18, 'label': 2},  # 2 people
     {'title': 'recording_2022_05_25_11_48_56',
-     'start': 200, 'end': 500, 'label': 3},  # 3 people
+     'start': 7, 'end': 18, 'label': 3},  # 3 people
 ]
 
 pesto1 = [
     {'title': 'recording_2022_05_25_19_06_11',
-     'start': 50, 'end': 1500, 'label': 0},  # 0 people
+     'start': 2, 'end': 50, 'label': 0},  # 0 people
     {'title': 'recording_2022_05_25_19_07_53',
-     'start': 20, 'end': 2700, 'label': 1},  # 1 person
+     'start': 1, 'end': 90, 'label': 1},  # 1 person
     {'title': 'recording_2022_05_25_19_10_22',
-     'start': 20, 'end': 3300, 'label': 2},  # 2 people
+     'start': 1, 'end': 110, 'label': 2},  # 2 people
     {'title': 'recording_2022_05_25_19_13_55',
-     'start': 110, 'end': 3300, 'label': 3},  # 3 people
+     'start': 4, 'end': 110, 'label': 3},  # 3 people
 ]
 
 bolognese1EPesto1 = bolognese1 + pesto1
 
+tonno = [
+    {"title": "recording_2022_05_26_11_08_08", 'start': 0, 'end': 4.5, 'label': 0},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 4.5, 'end': 62, 'label': 1},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 62, 'end': 98, 'label': 2},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 98, 'end': 109, 'label': 1},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 109, 'end': 135, 'label': 2},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 135, 'end': 150, 'label': 0},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 150, 'end': 188, 'label': 1},
+    {"title": "recording_2022_05_26_11_08_08",
+        'start': 188, 'end': 192, 'label': 0},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 192, 'end': 200, 'label': 1},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 200, 'end': 209, 'label': 0},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 209, 'end': 215, 'label': 1},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 215, 'end': 225, 'label': 0},
+    # {"title": "recording_2022_05_26_11_08_08",
+    #     'start': 225, 'end': 239, 'label': 1}
+]
 
 class SensorDataset(Dataset):
     def __init__(self, recording_list):
@@ -57,14 +87,54 @@ class SensorDataset(Dataset):
         return self.samples[idx]
 
 
-def preprocess_input(processed_data):
+class SensorCameraDataset(Dataset):
+    def __init__(self, recording_list):
+        self.samples = []
+
+        for recording in recording_list:
+            file_name = f'../../recordings/{recording["title"]}/RadarIfxAvian_00/radar.npy'
+            video_file = f'../../recordings/{recording["title"]}/CamOpenCV_00/rgb.mp4'
+            data = np.load(file_name)
+            cap = cv2.VideoCapture(video_file)
+
+            nOfDataFrames = data.shape[0]
+            nOfVideoFrames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+            def get_frame(video_frame):
+                k = nOfDataFrames/nOfVideoFrames
+                return int(video_frame * k)
+
+            start_frame, end_frame, labelix = get_frame(
+                recording["start"]*30), get_frame(recording["end"]*30), recording["label"]
+
+            label = torch.tensor([0, 0, 0, 0])
+            label[labelix] = 1
+
+            processed_data = processing.processing_rangeDopplerData(
+                data[start_frame:end_frame])
+
+            processed_data = preprocess_input(processed_data)
+
+            # iterate frames
+            for f in processed_data:
+
+                self.samples.append((f, label))
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        return self.samples[idx]
+
+
+def preprocess_input(processed_data, skip_gaussian=False):
     processed_data = np.abs(processed_data)
 
-    # processed_data[:, :, 32:34, :] = 0  # Remove the middle band (noise?)
+    processed_data[:, :, 32:34, :] = 0  # Remove the middle band (noise?)
 
-    for i in range(len(processed_data)):
-        for j in range(len(processed_data[i])):
-            processed_data[i, j] -= processed_data[i, j].mean()  # subtract the mean
+    # for i in range(len(processed_data)):
+    #     for j in range(len(processed_data[i])):
+    # processed_data[i, j] -= processed_data[i, j].mean()  # subtract the mean
             # processed_data[i, j] = gaussian_filter(processed_data[i, j], (1, 1))  # Gaussian blur
 
     processed_data[:, :, :, 45:] = 0  # Remove far away objects
